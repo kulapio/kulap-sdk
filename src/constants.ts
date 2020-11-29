35abc73f0fa265c1b4d1c006c2f83b8c892d8404
Pisuth D <pisuth.dae@gmail.com>
2020-11-29 16:37:34 +0700

    Feat: Add token addresses

--- src/constants.ts
+++ src/constants.ts
@@ -2,25 +2,27 @@ import { Network } from "./types"
 
 export const API_URL = "https://api.kulap.io/v1/api/rate/best-rate/toAmount"
 
-export const SUPPORTED_TOKENS = [
-    {
-        "symbol": "ETH",
-        "address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
-    },
-    {
-        "symbol": "USDT",
-        "address": "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-    },
-    {
-        "symbol": "DAI",
-        "address": "0x6B175474E89094C44Da98b954EedeAC495271d0F"
-    },
-    {
-        "symbol": "MAKER",
-        "address": "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2"
-    },
-    {
-        "symbol": "USDC",
-        "address": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-    }
-]
+export const SUPPORTED_TOKENS = [{ "code": "WBTC", "price": "17483.08059499", "change": "3.36741374985048523788" }, { "code": "OMG", "price": "3.651493605853668234", "change": "1.83939441453954003538" }, { "code": "COMP", "price": "105.63810067652973811", "change": "0.76062063039617354456" }, { "code": "SNX", "price": "4.184655627734776244", "change": "2.03984338045293011831" }, { "code": "LRC", "price": "0.189339548771068673", "change": "-2.3931006249197938885" }, { "code": "BUSD", "price": "0.991809900739853018", "change": "-0.0975453698822696428" }, { "code": "MKR", "price": "523.267687338374522362", "change": "2.14744929231966704198" }, { "code": "BAND", "price": "5.820326313460406007", "change": "0.45303696823388185114" }, { "code": "LINK", "price": "12.882423945769723304", "change": "2.96273320857849270503" }, { "code": "DGX", "price": "55.665725166444690462", "change": "-0.32881277256393515896" }, { "code": "USDC", "price": "0.99594256319189364", "change": "0.02325367167534316717" }, { "code": "ZRX", "price": "0.423964037039613148", "change": "5.3894533931064680138" }, { "code": "ETH", "price": "532.3674514395006", "change": "3.59722682299100083386" }, { "code": "DAI", "price": "1.003349", "change": "0.00877139647609146573" }, { "code": "KNC", "price": "1.172552586985660416", "change": "0" }, { "code": "BAT", "price": "0.226797465901862484", "change": "1.44230726539892270145" }, { "code": "USDT", "price": "0.996382897826282349", "change": "0.04327657922311353915" }]
+
+export const TOKEN_ADDRESSES  = {
+    "BAND": "0xba11d00c5f74255f56a5e366f4f77f5a186d7f55",
+    "BAT": "0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+    "BUSD": "0x4fabb145d64652a948d72533023f6e7a623c7c53",
+    "COMP": "0xc00e94cb662c3520282e6f5717214004a7f26888",
+    "DAI": "0x6b175474e89094c44da98b954eedeac495271d0f",
+    "DGX": "0x4f3afec4e5a3f2a6a1a411def7d7dfe50ee057bf",
+    "ETH": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    "KNC": "0xdd974d5c2e2928dea5f71b9825b8b646686bd200",
+    "LINK": "0x514910771af9ca656af840dff83e8264ecf986ca",
+    "LRC": "0xbbbbca6a901c926f240b89eacb641d8aec7aeafd",
+    "MKR": "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+    "OMG": "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07",
+    "SNX": "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f",
+    "USDC": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    "USDT": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    "WBTC": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+    "ZRX": "0xe41d2489571d322189246dafa5ebde1f4699f498"
+}
+
+export const KULAP_DEX_CONTRACT = "0x3833cf2972394d636b1C5b80d34FeE1F17175b77"
+
