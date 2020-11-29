2c6874183478a31d0a4eb32d7bda1b07178250d0
Pisuth D <pisuth.dae@gmail.com>
2020-11-29 16:36:56 +0700

    Feat: Implement jest integration tests /w forked mainnet

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,42 +1,67 @@
 import { Kulap } from "../src"
+import Web3 from "web3"
 
-let kulapSDK : any
+let kulapSDK: any
+let testRate : any
 
 describe("Kulap SDK", () => {
 
-    beforeAll(() => {
-        kulapSDK = new Kulap('access_key', "mainnet")
+    beforeAll(async () => {
+
+        const web3 = new Web3('http://localhost:8545')
+
+        kulapSDK = new Kulap('access_key',web3.currentProvider)
     });
 
-    test("Verify constructor creation is correct", async () => {
-        const networkName = kulapSDK.getCurrentNetwork()
-        expect(networkName).toBe("mainnet");
+    test("Local ganache is running and connected to mainnet" , async () => {
+        const networkId = await kulapSDK.getNetworkId()
+        expect(networkId).toBe(1)
+    })
 
-        try {
-            new Kulap('access_key', "wrong")
-        } catch (error) {
-            expect(error.message).toBe("Given network name is not valid.");
+    test("Verify supported tokens", async () => {
+        const symbols = kulapSDK.listSymbols()
+        expect(symbols.indexOf("ETH")).toBeGreaterThan(-1)
+        expect(symbols.indexOf("WBTC")).toBeGreaterThan(-1)
+        expect(symbols.indexOf("OMG")).toBeGreaterThan(-1)
+    })
+
+    test("Verify returned rates and routes from off-chain API", async () => {
+        const symbols : string[] = kulapSDK.listSymbols()
+        const baseToken = symbols.find(symbol => symbol === "ETH")
+        const pairToken = symbols.find(symbol => symbol === "DAI")
+        const amountIn = "1000000000000000000" // 1 ETH
+        testRate = await kulapSDK.getRate(baseToken, pairToken, amountIn)
+        expect(testRate.routes.length).toBeGreaterThan(0)
+        expect(testRate.fromAmount).toBe(amountIn)
+    
+    })
+
+    test("Verify there is trading proxies", async () => {
+        const totalProxies = await kulapSDK.totalProxies()
+        expect(totalProxies).toBeGreaterThan(0)
+    })
+
+    test("Verify validation and approve fuctions are functional", async () => {
+        const symbols : string[] = kulapSDK.listSymbols()
+        const baseToken = symbols.find(symbol => symbol === "DAI")
+        const pairToken = symbols.find(symbol => symbol === "ETH")
+        const amountIn = "1000000000000000000" // 1 DAI
+        const rateForApprove = await kulapSDK.getRate(baseToken, pairToken, amountIn)
+        const validated = await kulapSDK.validate(rateForApprove)
+        if (!validated) {
+            await kulapSDK.approve(rateForApprove, "FAST")
         }
+        const afterApproval = await kulapSDK.validate(rateForApprove)
+        expect(afterApproval).toBe(true)
+    })
 
-    });
-
-    test("Verify supported tokens list is correct", async () => {
-        const symbols = kulapSDK.tokensList()
-        expect( symbols.indexOf("ETH") ).toBeGreaterThan(-1)
-        expect( symbols.indexOf("DAI") ).toBeGreaterThan(-1)
-    });
-
-
-    test("Verify returned rates and paths are received ", async () => {
-        const baseToken = kulapSDK.tokensList()[0]
-        const pairToken = kulapSDK.tokensList()[1]
-        const amountIn = "1000000000000000000"
-        const txOptions = await kulapSDK.getRate(baseToken, pairToken, amountIn)
-        expect( Object.keys(txOptions).length).toBe(3)
-        expect( txOptions.STD.trade.fromAmount ).toBe(amountIn)
-    });
-
-    
-    
+    test("Verify trade execution", async () => {
+        
+        const beforeSwap = await kulapSDK.balanceOf("DAI")
+        await kulapSDK.trade(testRate)
+        const afterSwap = await kulapSDK.balanceOf("DAI")
+        expect(afterSwap).toBeGreaterThan(beforeSwap)
+        console.log("before / after ", `${beforeSwap} DAI`, `${afterSwap} DAI`)
+    })
 
 });
